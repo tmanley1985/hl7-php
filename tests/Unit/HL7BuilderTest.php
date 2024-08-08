9b44682178916cbf59ee7cf588bf71bcd5345868
Thomas Manley <tmanley1985@gmail.com>
2024-08-08 00:30:53 -0400

    Adding basic test

--- tests/Unit/HL7BuilderTest.php
+++ tests/Unit/HL7BuilderTest.php
@@ -2,10 +2,94 @@
 
 namespace TManley1985\HL7Php\Tests\Unit;
 
+use TManley1985\HL7Php\Messages\HL7v2Message;
 use TManley1985\HL7Php\Tests\Stubs\HL7Stubs;
 
 test('example', function () {
 
-    var_dump(HL7Stubs::getADT_A01());
-    expect(true)->toBeTrue();
+    $expected = HL7Stubs::getADT_A01();
+    $message = HL7v2Message::builder()
+        ->addSegment('MSH', function ($segment) {
+            $segment
+                ->addField("^~\&")
+                ->addField("SENDING_APPLICATION")
+                ->addField("SENDING_FACILITY")
+                ->addField("RECEIVING_APPLICATION")
+                ->addField("RECEIVING_FACILITY")
+                ->addField("20230815083000")
+                ->addEmptyFields(1)
+                ->addField("ADT^A01")
+                ->addField("MSG00001");
+        })
+        ->addSegment("EVN", function ($segment) {
+            $segment
+                ->addField("A01")
+                ->addField("20230815083000");
+        })
+        ->addSegment("PID", function ($segment) {
+            $segment
+                ->addField("1")
+                ->addEmptyFields(1)
+                ->addField("10006579^^^1^MRN^1")
+                ->addEmptyField()
+                ->addField("DUCK^DONALD^D")
+                ->addEmptyFields(1)
+                ->addField("19241010")
+                ->addField("M")
+                ->addEmptyFields(1)
+                ->addField("1")
+                ->addField("111 DUCK ST^^FOWL^CA^999990000^^M")
+                ->addField("1")
+                ->addField("8005551212")
+                ->addField("8005551212")
+                ->addField("1")
+                ->addField("2")
+                ->addEmptyField(1)
+                ->addField("40007716^^^1^AN^1")
+                ->addEmptyFields(1)
+                ->addField("610074^^^1^DD^1")
+                ->addField("Y")
+                ->addField("1")
+                ->addEmptyFields(10)
+                ->addField("N");
+        })
+        ->addSegment("NK1", function ($segment) {
+            $segment
+                ->addField("1")
+                ->addField("DUCK^DAISY^D")
+                ->addField("SO")
+                ->addField("3583 DUCK RD^^FOWL^CA^999990000")
+                ->addField("8005552222")
+                ->addEmptyFields(1)
+                ->addField("Y")
+                ->addEmptyFields(13);
+        })
+        ->addSegment("PV1", function ($segment) {
+            $segment
+                ->addField("1")
+                ->addField("I")
+                ->addField("PREOP^101^1^1^^^S")
+                ->addField("3")
+                ->addEmptyFields(2)
+                ->addField("37^DISNEY^WALT^^^^^^1")
+                ->addEmptyFields(2)
+                ->addField("01")
+                ->addEmptyFields(3)
+                ->addField("1")
+                ->addEmptyFields(2)
+                ->addField("37^DISNEY^WALT^^^^^^1")
+                ->addField("2")
+                ->addField("40007716^^^1^AN^1")
+                ->addField("4")
+                ->addEmptyFields(18)
+                ->addField("1")
+                ->addEmptyFields(1)
+                ->addField("G")
+                ->addEmptyFields(2)
+                ->addField("20230815082500")
+                ->addEmptyFields(5);
+        })
+        ->build();
+
+    expect($message->toString())->toBe(HL7Stubs::getADT_A01());
 });
